7280164b6de3fdea80d9a849cd4d9e44e87134af
junwoo45 <sinyoorang@gmail.com>
2019-12-16 15:51:26 +0900

    Reactive form TEST && Adjust prettier

--- reactive-form/src/app/app.component.ts
+++ reactive-form/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule
+} from '@angular/forms';
 
+declare var NgForm: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive-form';
+export class AppComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
+  title = 'Reactive form validation in Angular 8!';
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+      tnc: ['', Validators.required]
+    });
+  }
+
+  get fval() {
+    return this.registerForm.controls;
+  }
+
+  signup() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    alert('form fields are validated succe3ssfully!!!!!');
+  }
 }
